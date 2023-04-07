df4f1c63616741a67142aa87c425b6944578561b
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-07 23:58:13 +0530

    Added : Code for print duplicate array element

---
+++ src/com/arraypracticeproblem/DuplicateElement.java
@@ -0,0 +1,25 @@
+package com.arraypracticeproblem;
+
+import java.util.Scanner;
+
+public class DuplicateElement {
+    // program to print the duplicate array element;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the element");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    System.out.println(array[i]);
+                }
+            }
+        }
+    }
+}
